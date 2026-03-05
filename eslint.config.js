84a3e86ee4b7246bc39df6349fa4e9286ea334f6
boileroo <boileroo88@googlemail.com>
2026-03-05 07:01:49 +0000

    fix: prettier

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,15 @@ import tseslint from 'typescript-eslint';
 import prettier from 'eslint-config-prettier';
 
 export default tseslint.config(
-  { ignores: ['dist', 'node_modules', '.output', '.vinxi'] },
+  {
+    ignores: [
+      'dist',
+      'node_modules',
+      '.output',
+      '.vinxi',
+      'src/routeTree.gen.ts',
+    ],
+  },
   {
     extends: [
       js.configs.recommended,
